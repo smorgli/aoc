21bcdfeb6db49c9ce469b60c81ad06a88ec4648f
PatrykP <PatrykPolomski@o-i.com>
2022-12-02 08:44:09 +0100

    better read

--- funs.py
+++ funs.py
@@ -2,7 +2,12 @@ import os
 
 INPUT_FOLDER = 'inputs'
 
-def getfile(filename):
+def getfile(filename,split = True):
     with open(str(os.path.join(INPUT_FOLDER,filename))) as f:
-        lines = f.readlines()
+        if split:
+            lines = f.read().splitlines()
+        else:
+            lines = f.readlines()
     return(lines)
+
+#print(getfile('input2.txt'))
